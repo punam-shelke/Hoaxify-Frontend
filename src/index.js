1299c09221e13e3d215a5cfc79739568e4b06f65
punam-shelke <poonam.shelke@thoughtworks.com>
2021-01-09 19:46:44 +0530

    Add UserSignupPage to show up in frontend

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import UserSignUpPage from "./pages/UserSignupPage";
+import { UserSignupPage } from "./pages/UserSignupPage";
 ReactDOM.render(
   <React.StrictMode>
-    <UserSignUpPage />
+    <UserSignupPage />
   </React.StrictMode>,
   document.getElementById("root")
 );
